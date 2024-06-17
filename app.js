d437c0ac080f722c246c3dc338b60f0b6b5beb7e
vusal965 <vusalbaxsiyev756@gmail.com>
2024-06-17 14:20:01 +0400

    Home work

---
+++ app.js
@@ -0,0 +1,14 @@
+let fullName = "Vusal Baxsiyev"
+console.log(fullName.length -1);
+
+
+
+
+
+let text = "Vusal baku code centerde tehsil alir"
+console.log(text);
+console.log(text.length);
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.replaceAll("t","####"));
